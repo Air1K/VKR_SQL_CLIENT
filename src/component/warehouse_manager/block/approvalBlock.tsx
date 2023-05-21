8fe11c82620b5c38f330ed1fa749cc56f8d7b7e4
Air1K <veremeenkom91@gmail.com>
2023-05-21 21:47:10 +0500

    Утверждение

--- src/component/warehouse_manager/block/approvalBlock.tsx
+++ src/component/warehouse_manager/block/approvalBlock.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import styles from '../../meny/plan/nodeAndZon/inputBlock/stylesBlock.module.sass'
+import Selected from "../../tag/select/select";
+import {Context} from "../../../index";
 
 const ApprovalBlock = () => {
+    console.log("Рендер ApprovalBlock ____________________________________")
+    const {store} = useContext(Context);
+    const [active, setActive] = useState(null)
     return (
         <div className={styles.main}>
             <div className={styles.oknovvoda}>
                 <h6>Утверждение склада</h6>
-                <input type="text"/>
-                Выберите склад:
-                <select className="browser-default" style={{
-                    backgroundColor: "rgb(255 255 255 / 0%)",
-                    border: '1px solid #f2f2f200',
-
-                }}>
-                </select>
+                Выберите статус утверждения:
+                <Selected active={active} setActive={setActive} nameLabel={"Выберите утверждение"} objMap={store.approval} ID={"id_status"}/>
+                <br/><br/>
+                <button>Сохранить</button>
             </div>
         </div>
     );
